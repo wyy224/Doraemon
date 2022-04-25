9b76436a4eb874a0daec8c0b77ef6889f6858caf
Peixuan Dong <peixuan.dong@ucdconnect.ie>
2022-04-25 15:27:02 +0800

    app socketIO

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import os
+from socket import SocketIO
+
+from flask import Flask
 
 from app import app, db
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123'
+
+socketio = SocketIO()
+socketio.init_app(app)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    # app.run(debug=True)
