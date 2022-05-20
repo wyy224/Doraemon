91800d073c67e4d17c61d7987dbbe94c47c7f481
Peixuan Dong <peixuan.dong@ucdconnect.ie>
2022-05-20 12:46:07 +0800

    change

--- app/routes.py
+++ app/routes.py
@@ -620,19 +620,23 @@ def change(p):
 def collect():
     user_id = request.form.get("user_id")
     commodity_id = request.form.get("commodity")
-    exist = Collections.query.filter_by(user_id=user_id, commodity_id=commodity_id).first()
-    commodity5 = Commodity.query.filter_by(id=commodity_id).first()
-    if exist is not None:
-        commodity5.collect_num = commodity5.collect_num - 1
-        db.session.delete(exist)
-        db.session.commit()
-        return "cancel collect"
+    user = User.query.filter(User.id == user_id).first()
+    if user.ban == 0:
+        exist = Collections.query.filter_by(user_id=user_id, commodity_id=commodity_id).first()
+        commodity5 = Commodity.query.filter_by(id=commodity_id).first()
+        if exist is not None:
+            commodity5.collect_num = commodity5.collect_num - 1
+            db.session.delete(exist)
+            db.session.commit()
+            return "cancel collect"
+        else:
+            commodity5.collect_num = commodity5.collect_num + 1
+            collect1 = Collections(user_id=user_id, commodity_id=commodity_id)
+            db.session.add(collect1)
+            db.session.commit()
+            return "collect success"
     else:
-        commodity5.collect_num = commodity5.collect_num + 1
-        collect1 = Collections(user_id=user_id, commodity_id=commodity_id)
-        db.session.add(collect1)
-        db.session.commit()
-        return "collect success"
+        return "This user has been disabled, unable to collect!"
 
 
 @app.route('/adjust_icon', methods=['GET', 'POST'])
